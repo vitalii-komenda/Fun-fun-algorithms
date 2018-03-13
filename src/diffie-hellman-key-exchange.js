f4c0f1c4a1028c5c0792369cb4088e95b1bb9c27
Vitaliy Komenda <imorfie@gmail.com>
2018-03-13 23:25:34 +0200

    Update diffie-hellman-key-exchange.js

--- src/diffie-hellman-key-exchange.js
+++ src/diffie-hellman-key-exchange.js
@@ -1,17 +1,22 @@
-const dhKeyExchange = {
-  dividend: 3,
-  divisor: 17,
-
-  modulo(exp = 1) {
-    return this.dividend ** exp % this.divisor;
-  },
-  createTwoPublicKeys(secretKey1, secretKey2) {  
-    return {
-      pubKey1: this.modulo(secretKey1),
-      pubKey2: this.modulo(secretKey2)
+const dhKeyExchange = function(dividend=3, divisor=17) {
+  return {
+    modulo(exp = 1) {
+      return dividend ** exp % divisor;
+    },
+    createTwoPublicKeys(secretKey1, secretKey2) {  
+      return {
+        pubKey1: this.modulo(secretKey1),
+        pubKey2: this.modulo(secretKey2)
+      }
+    },
+    getSharedSecret(pubKey, secretKey) {
+      return pubKey ** secretKey % divisor;
     }
-  },
-  getSharedSecret(pubKey, secretKey) {
-    return pubKey ** secretKey % this.divisor;
   }
 }
+
+
+// const obj = dhKeyExchange();
+// const res = obj.createTwoPublicKeys(6, 17);
+// obj.getSharedSecret(6, res.pubKey2); // 6
+// obj.getSharedSecret(17, res.pubKey1); // 6 the same
