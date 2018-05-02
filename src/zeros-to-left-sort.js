286382865583e65ad24297cb53db8318aa2a3ccb
Vitaliy Komenda <imorfie@gmail.com>
2018-05-02 13:15:57 +0300

    Update zeros-to-left-sort.js

--- src/zeros-to-left-sort.js
+++ src/zeros-to-left-sort.js
@@ -1,35 +1,35 @@
 const swap = (arr, from, to) => {
-	let tmp = arr[from];
-	arr[from] = arr[to];
-	arr[to] = tmp;
+  let tmp = arr[from];
+  arr[from] = arr[to];
+  arr[to] = tmp;
 };
 
 const findNonZero = (arr, from) => {
-	for(let i=from; i<arr.length; i++) {
-		if (arr[i] !== 0) {
-			return i;
-        }
+  for(let i=from; i<arr.length; i++) {
+    if (arr[i] !== 0) {
+      return i;
     }
-	return -1;
+  }
+  return -1;
 }
 
 const sort = (arr) => {
-	let i = 0;
-	while(i < arr.length) {
-		if (arr[i] === 0) {
-			let indx = findNonZero(arr, i);
+  let i = 0;
+  while(i < arr.length) {
+    if (arr[i] === 0) {
+      let indx = findNonZero(arr, i);
 
-			if (indx === -1) {
-				break;
-            } else {
-				swap(arr, i, indx);
-            }
-        }
-
-		i++;
+      if (indx === -1) {
+        break;
+      } else {
+        swap(arr, i, indx);
+      }
     }
 
-	return arr;
+    i++;
+  }
+
+  return arr;
 };
 
 
