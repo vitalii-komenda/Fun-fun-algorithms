db73912d23e52c5260f0196cc80af3c0fa004387
Vitaliy Komenda <imorfie@gmail.com>
2018-08-22 10:41:28 +0300

    Update longest-common-subsequence.js

--- src/longest-common-subsequence.js
+++ src/longest-common-subsequence.js
@@ -6,12 +6,12 @@ export const lcs = (p, q, n, m, res = []) => {
   if (n === 0 || m === 0) {
     return [0, res];
   } else if (p[n-1] === q[m-1]) {
-    let r = lcs(p, q, n-1, m-1, [q[m-1]].concat(res));
+    const r = lcs(p, q, n-1, m-1, [q[m-1]].concat(res));
     return [1 + r[0], r[1]];
   } else {
-    let tmp1 = lcs(p, q, n-1, m, res);
-    let tmp2 = lcs(p, q, n, m-1, res);
+    const tmp1 = lcs(p, q, n-1, m, res);
+    const tmp2 = lcs(p, q, n, m-1, res);
 
     return tmp1[0] > tmp2[0] ? tmp1 : tmp2;
   }
-}
+};
