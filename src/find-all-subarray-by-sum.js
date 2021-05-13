4a7d8b116ed9168c9c5296be83558c123b32a5c9
Vitalii Komenda <imorfie@gmail.com>
2021-05-13 11:28:49 +0300

    Create find-all-subarray-by-sum.js

---
+++ src/find-all-subarray-by-sum.js
@@ -0,0 +1,27 @@
+const arr = [12, 1, 2,3,7 ,5, 12];
+const s = 12;
+
+const flsa = (a, s) => {
+    let p1 = 0;
+    let p2 = 0
+    let sum = a[p1];
+
+    let res = [];
+
+    while(p1 < a.length) {
+        if (sum === s) {
+            res.push([p1, p2-1 < 0 ? 0 : p2-1]);
+            sum -= a[p1];
+            p1++;
+            if (p2<p1) p2 = p1; 
+        } else if (sum < s) {
+            sum += a[p2];
+            if (p2 < a.length) p2++;
+        } else if (sum > s) {
+            sum -= a[p1];
+            p1++;
+        }
+    }
+    return res;
+};
+// flsa(arr, s)
