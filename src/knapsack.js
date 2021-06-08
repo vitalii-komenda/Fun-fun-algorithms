dd956963e96bcefd2db87ca2c071b22852270046
Vitalii Komenda <imorfie@gmail.com>
2021-06-08 14:23:50 +0300

    Create knapsack.js

---
+++ src/knapsack.js
@@ -0,0 +1,18 @@
+items=[{v: 5, w: 10}, {v: 1, w: 1}, {v: 3, w: 4}]
+
+knp = (items, selected, maxWeight, val)=>{
+    if (maxWeight === 0) return [val, selected];
+    if (maxWeight < 0) return [0, selected];
+    if (!items.length) return [val, selected];
+
+    let last = items[items.length-1];
+    let including = knp(
+      items.slice(0, -1),
+      selected.concat(last),
+      maxWeight - last.w,
+      val+last.v
+    );
+    let notIncluding = knp(items.slice(0, -1), selected, maxWeight, val);
+    
+    return including[0] > notIncluding[0] ? including : notIncluding;
+}
