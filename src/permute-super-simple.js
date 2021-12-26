d931ff63e70041c850fe4a99d684bfbe8ea1ca43
Vitalii Komenda <imorfie@gmail.com>
2021-12-26 20:26:39 +0200

    Update permute-super-simple.js

--- src/permute-super-simple.js
+++ src/permute-super-simple.js
@@ -8,3 +8,27 @@ const p = (arr, res = [], ans) => {
         p(slice(arr, i), res.concat(arr[i]), ans)
     }
 }
+
+const p2 = function (nums) {
+  const result = [];
+  // TODO: Write your code here
+
+  const f = (localNums, res) => {
+    if (res.length >= nums.length || !localNums.length) {
+      result.push(res);
+
+      return;
+    }
+
+    for (let i=0; i < localNums.length; i++) {
+      const copy = localNums.slice(0);
+      copy.splice(i, 1);
+
+      f(copy, [localNums[i]].concat(res.slice(0)));
+    }
+  }
+
+  f(nums, []);
+
+  return result;
+};
